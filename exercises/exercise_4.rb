586a36b316c50ac8e6c1e4020a67d219a53f9503
Friies <bahram.parsa2019@gmail.com>
2023-08-14 13:31:02 -0400

    Complete exercise 3-5. - exercise 3: find and delete a 3rd store. Display the count. - exercise 4: add 3 more stores and filter annual revenue, men/women apparel. - exercise 5: calculate and output total revenue.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,33 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
+=begin
+Exercise 4: Loading a subset of stores
+  1. Borrowing and modifying the code from Exercise one, create 3 more stores:
+    - Surrey (annual_revenue of 224000, carries women's apparel only)
+    - Whistler (annual_revenue of 1900000 carries men's apparel only)
+    - Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+  2. Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
+  3. Loop through each of these stores and output their name and annual revenue on each line.
+  4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+=end
+
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+# This line returns an ActiveRecord relation, which is a collection of records that satisfy the specified condition (stores carrying men's apparel).
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.find_each do |store|
+  puts "#{store.name} has a annual revenue of #{store.annual_revenue}."
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+
+@womens_stores.find_each do |store|
+  puts "#{store.name} has a annual revenue of #{store.annual_revenue}."
+end
