ff050b2d18e84b166748903ed67c3c6e90184e3b
Friies <bahram.parsa2019@gmail.com>
2023-08-14 13:29:47 -0400

    Update store name and display it.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,20 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
+=begin
+Exercise 2: Update the first store
+  1. Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+  2. Load the second store from the database and assign it to @store2.
+  3. Update the first store (@store1) instance in the database. (Change its name or something.)
+=end
+
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+
+@store1.name = "CF Mall"
+@store1.save
+puts "The first stores name is #{@store1.name}"
